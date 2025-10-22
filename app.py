39671d5add04664a48c3844e4c73049533c51275
Mert Erbak <merterbak@users.noreply.huggingface.co>
2025-10-22 12:54:19 +0000

    Update app.py

--- app.py
+++ app.py
@@ -223,7 +223,7 @@ with gr.Blocks(theme=gr.themes.Soft(), title="DeepSeek-OCR") as demo:
     # 🚀 DeepSeek-OCR
     **Convert documents to markdown, extract raw text, and locate specific content with bounding boxes. Check the info at the bottom of the page for more information.**
     
-    *Hope this tool was helpful! If so, a quick like ❤️ would mean a lot :)*
+    **Hope this tool was helpful! If so, a quick like ❤️ would mean a lot :)**
     """)
     
     with gr.Row():
